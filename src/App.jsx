8506b44c7c2a186b03027fd7ee67387dad70a230
muslimsislomjonova-cmd <muslimsislomjonova@gmail.com>
2026-02-06 17:15:12 +0500

    weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,65 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const apiKey = "46351da790226c653537b9628dc20463";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
 function App() {
+  const [city, setCity] = useState("");      
+  const [weather, setWeather] = useState(null); 
+  const [error, setError] = useState("");       
+
+  async function getWeather(cityName) {
+    try {
+      const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
+
+      if (response.status === 404) {
+        setWeather(null);
+        setError("City not found");
+        return;
+      }
+
+      const data = await response.json();
+      setWeather(data);
+      setError("");
+    } catch (err) {
+      console.log(err);
+      setError("Nmadur xato");
+    }
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      getWeather(city);
+    }
+  };
+
   return (
-    <div>
-      <header style={{ background: "#282c34", color: "white", padding: "20px" }}>
-        <h1>Mening birinchi react saytim</h1>
-        <p>1-page website</p>
-      </header>
-      
-      <main style={{ padding: "20px" }}>
-        <section>
-          <h2>Biz haqimizda</h2>
-          <p>
-            bu sayt reactda qlngan br sahifali web sayt.
-          </p>
-          
-        </section>
+    <div className="app">
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Enter city..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        onKeyDown={handleKeyDown}
+        style={{ padding: "10px", width: "200px" }}
+      />
 
-        <section>
-          <h2>Xizmatlar</h2>
-          <ul>
-            <li>web dizayn</li>
-           <li>frontend</li>
-            <li>react loyihalar</li>
-          </ul>
-        </section>
+      <div className="clouds" >
+        {error && <p>{error}</p>}
 
-        <section>
-          <h2>Aloqa</h2>
-          <p>Email: example@gmail.com</p>
-        </section>
-      </main>
-
-      <footer style={{ background: "#eee", padding: "10px", textAlign: "center" }}>
-        <p> 2026 mening saytim</p>
-      </footer>
+        {weather && (
+          <div>
+            <h1 className="city-name">{weather.name}</h1>
+            <p className="temperature">Temperature: {Math.round(weather.main.temp)}°C</p>
+            <p className="weather">Weather: {weather.weather[0].main}</p>
+            <p className="pressure">Pressure: {weather.main.pressure} hPa</p>
+            <p className="speed">Wind speed: {weather.wind.speed} km/h</p>
+            <p className="humidity">Humidity: {weather.main.humidity}%</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
